072a9fd86e5d8421d945dd33fa00b327d94c61c8
Hugo Lomba <hmlomba@gmail.com>
2022-09-20 02:32:54 -0300

    add responsividade nas paginas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,27 @@
 import "./Navbar.css";
 import { NavLink, Link } from "react-router-dom";
 import { BsInfoCircleFill } from "react-icons/bs";
+import { ImMenu } from "react-icons/im";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { useState } from "react";
 
 const Navbar = ({ planets }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  const handleMenu = () => setMenuIsOpen(!menuIsOpen);
+
   return (
     <nav className="navbar">
       <Link to="/">
         <h1>The Solar System</h1>
       </Link>
-      <ul>
+      <ul className={menuIsOpen ? `` : `nav-invisible`}>
         {planets.length > 0 &&
           planets.map((planeta) => {
             return (
               <li key={planeta.id}>
                 <NavLink
+                  onClick={handleMenu}
                   className={`planet${planeta.id}`}
                   to={`/planeta/${planeta.id}`}
                 >
@@ -29,6 +37,13 @@ const Navbar = ({ planets }) => {
           <BsInfoCircleFill />
         </Link>
       </ul>
+      <div>
+        {menuIsOpen ? (
+          <AiFillCloseCircle className="close-menu-btn" onClick={handleMenu} />
+        ) : (
+          <ImMenu className="open-menu-btn" onClick={handleMenu} />
+        )}
+      </div>
     </nav>
   );
 };
